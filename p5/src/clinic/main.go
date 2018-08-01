0b14efe2cfcd419ae5b6b399de9779de7e8d1b6a
manan <aoy.mananchaya@gmail.com>
2018-08-01 17:32:34 +0700

    [Edited]:แยก ฟังก์ชัน setupRouter

--- p5/src/clinic/main.go
+++ p5/src/clinic/main.go
@@ -6,8 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	router := gin.Default()
 	router.POST("/api/v1/patients", handler.CreateNewPatient)
+	return router
+}
+
+func main() {
+	router := setupRouter()
 	router.Run(":8080")
 }
