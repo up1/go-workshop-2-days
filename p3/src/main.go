82f9b96230e257f72448f5e93ec99d30b166688d
Apipol Sukgler <apipol@computerlogy.com>
2018-08-01 10:58:14 +0700

    [Edited]: move out handler and service out of main, should transform to struct api and service

--- p3/src/main.go
+++ p3/src/main.go
@@ -25,62 +25,73 @@ type Patient struct {
 	Age       int           `json:"age"bson:"age"`
 }
 
+type Api struct {
+	PatientService PatientService
+}
+
+type PatientService struct {
+	DBSession *mgo.Session
+}
+
 func main() {
 	DBSession, err := mgo.Dial(url)
 	if err != nil {
 		fmt.Println("Cannot connect database ", err.Error())
 		return
 	}
-
-	http.HandleFunc("/v1/patients", func(writer http.ResponseWriter, request *http.Request) {
-		var patient Patient
-		err := json.NewDecoder(request.Body).Decode(&patient)
-		if err != nil {
-			http.Error(writer, "Not Found", http.StatusNotFound)
-			return
-		}
-		newPatient, err := InsertPatient(DBSession, patient)
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(writer).Encode(newPatient)
-	})
+	patientService := PatientService{DBSession: DBSession}
+	api := Api{PatientService: patientService}
+	http.HandleFunc("/v1/patients", api.CreatePatientHandler)
 
 	http.ListenAndServe(":3000", nil)
 
 }
 
-func InsertPatient(session *mgo.Session, patient Patient) (Patient, error) {
+func (api Api) CreatePatientHandler(writer http.ResponseWriter, request *http.Request) {
+	var patient Patient
+	err := json.NewDecoder(request.Body).Decode(&patient)
+	if err != nil {
+		http.Error(writer, "Not Found", http.StatusNotFound)
+		return
+	}
+	newPatient, err := api.PatientService.InsertPatient(patient)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(writer).Encode(newPatient)
+}
+
+func (service PatientService) InsertPatient(patient Patient) (Patient, error) {
 	var newPatient Patient
-	patient.PatientID, _ = GeneratePatientID(session)
-	err := session.DB("hospital_somkiat").C("patients").Insert(patient)
+	patient.PatientID, _ = service.GeneratePatientID()
+	err := service.DBSession.DB("hospital_somkiat").C("patients").Insert(patient)
 	if err != nil {
 		return patient, err
 	}
-	UpdatePatientCount(session)
-	err = session.DB("hospital_somkiat").C("patients").Find(bson.M{"patientID": patient.PatientID}).One(&newPatient)
+	service.UpdatePatientCount()
+	err = service.DBSession.DB("hospital_somkiat").C("patients").Find(bson.M{"patientID": patient.PatientID}).One(&newPatient)
 	return newPatient, err
 }
 
-func GeneratePatientID(session *mgo.Session) (string, error) {
+func (service PatientService) GeneratePatientID() (string, error) {
 	var patientCount PatientCount
-	err := session.DB("hospital_somkiat").C("patientsCount").Find(nil).One(&patientCount)
+	err := service.DBSession.DB("hospital_somkiat").C("patientsCount").Find(nil).One(&patientCount)
 	if err != nil && err.Error() == "not found" {
 		patientCount.Count = 0
-		session.DB("hospital_somkiat").C("patientsCount").Insert(&patientCount)
+		service.DBSession.DB("hospital_somkiat").C("patientsCount").Insert(&patientCount)
 	}
 	patientCount.Count++
-	return FormatPatientID(time.Now().Year(), patientCount.Count), nil
+	return service.FormatPatientID(time.Now().Year(), patientCount.Count), nil
 }
 
-func UpdatePatientCount(session *mgo.Session) {
+func (service PatientService) UpdatePatientCount() {
 	var patientCount PatientCount
-	session.DB("hospital_somkiat").C("patientsCount").Find(nil).One(&patientCount)
-	session.DB("hospital_somkiat").C("patientsCount").UpdateId(patientCount.ID, bson.M{"$inc": bson.M{"count": 1}})
+	service.DBSession.DB("hospital_somkiat").C("patientsCount").Find(nil).One(&patientCount)
+	service.DBSession.DB("hospital_somkiat").C("patientsCount").UpdateId(patientCount.ID, bson.M{"$inc": bson.M{"count": 1}})
 }
 
-func FormatPatientID(year, number int) string {
+func (service PatientService) FormatPatientID(year, number int) string {
 	return fmt.Sprintf("%d-%04d", year, number)
 }
