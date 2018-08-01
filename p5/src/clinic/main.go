771a6a2390fba6b5ae56e9647410aacf39214e20
manan <aoy.mananchaya@gmail.com>
2018-08-01 16:40:09 +0700

    [Created]: สร้าง โฟลเดอร์ clinic เพื่อเป็น package หลัก

---
+++ p5/src/clinic/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"clinic/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.POST("/api/v1/patients", handler.CreateNewPatient)
+	router.Run(":8080")
+}
