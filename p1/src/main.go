dbda976922d39ddf789eae3f281fadf24e1ff93e
smalldog124 <panumars@gmail.com>
2018-08-01 15:44:25 +0700

    [Created]:แยก router ออกจาก main

--- p1/src/main.go
+++ p1/src/main.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"log"
+	"router"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
-	router.POST("api/v1/patient", routerHandler)
-	router.Run(":8080")
-}
-
-type patientResponse struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-type patientRequest struct {
-	ID    string `json:"id"`
-	Fname string `json:"fname"`
-	Lname string `json:"lname"`
-	Age   int    `json:"age"`
-}
-
-func routerHandler(c *gin.Context) {
-	var patientRequest patientRequest
-	err := c.ShouldBind(&patientRequest)
-	if err != nil {
-		log.Println("bind error")
-	}
-	patient := patientResponse{
-		Id:   "2018-0001",
-		Name: patientRequest.Fname + patientRequest.Lname,
-		Age:  patientRequest.Age,
-	}
-	c.JSON(201, patient)
+	r := gin.Default()
+	r.POST("api/v1/patient", router.RouterCreate)
+	r.Run(":8080")
 }
