026d9ffa05e4e2ee9065944e6c6af4012121b239
Apipol Sukgler <apipol@computerlogy.com>
2018-08-01 10:00:10 +0700

    [Edited]: add struct patients for receive and send data in api

--- p3/src/main.go
+++ p3/src/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
+type Patient struct {
+	ID        string `json:"id,omitempty"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
 func main() {
-	http.HandleFunc("/v1/patients", func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Not Found", http.StatusNotFound)
+	http.HandleFunc("/v1/patients", func(writer http.ResponseWriter, request *http.Request) {
+		var patient Patient
+		err := json.NewDecoder(request.Body).Decode(&patient)
+		if err != nil {
+			http.Error(writer, "Not Found", http.StatusNotFound)
+		}
+
+		json.NewEncoder(writer).Encode(patient)
 	})
 }
