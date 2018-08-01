96bcf47f019c1e2464418a71109a09bf877e1fae
smalldog124 <panumars@gmail.com>
2018-08-01 15:19:02 +0700

    [Edited]: response name ให้เป็น fname+lname

--- p1/src/main.go
+++ p1/src/main.go
@@ -13,13 +13,12 @@ func main() {
 }
 
 type patientResponse struct {
-	Id    string `json:"id"`
-	Fname string `json:"fname"`
-	Lname string `json:"lname"`
-	Age   int    `json:"age"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 type patientRequest struct {
-	Id    string `json:"id"`
+	ID    string `json:"id"`
 	Fname string `json:"fname"`
 	Lname string `json:"lname"`
 	Age   int    `json:"age"`
@@ -32,10 +31,9 @@ func routerHandler(c *gin.Context) {
 		log.Println("bind error")
 	}
 	patient := patientResponse{
-		Id:    "2018-0001",
-		Fname: "panumars",
-		Lname: "seanto",
-		Age:   patientRequest.Age,
+		Id:   "2018-0001",
+		Name: patientRequest.Fname + patientRequest.Lname,
+		Age:  patientRequest.Age,
 	}
 	c.JSON(201, patient)
 }
