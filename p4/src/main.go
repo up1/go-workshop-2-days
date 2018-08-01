7ca55dfb4edd760cf89430285cb86b0780c0ffa6
platylm <n_nareenart@kkumail.com>
2018-08-01 14:57:03 +0700

    [Edited]:add routerHandle by gin

--- p4/src/main.go
+++ p4/src/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"log"
-	"member"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
+type patientsResponse struct {
+	Id        string `json:"patient_id"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
 func main() {
-	http.HandleFunc("/patient", member.GetPatient)
-	http.HandleFunc("/patient/insert", member.Insert)
-	log.Println("Server running on port 3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	router := gin.Default()
+	router.POST("/api/v1/patient", routerHandle)
+	router.Run(":3000")
+}
+
+func routerHandle(c *gin.Context) {
+	patient := patientsResponse{
+		Id:        "2018-0001",
+		Firstname: "นารีนารถ",
+		Lastname:  "เนรัญชร",
+		Age:       22,
+	}
+	c.JSON(200, patient)
 }
