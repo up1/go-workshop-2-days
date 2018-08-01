6a9fcf8b3dc7bba283aed0a8bb4c31715884e85b
ploy <chonnikan.t@kkumail.com>
2018-08-01 15:23:24 +0700

    [Added]:comment การทำงานของแต่ละตัว

--- p2/src/handler/apihandler.go
+++ p2/src/handler/apihandler.go
@@ -4,12 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Request รูปแบบของสิ่งที่จะรับเข้ามา
 type Request struct {
 	FirstName string
 	LastName  string
 	Age       int
 }
 
+// Response ของที่จะส่งกลับไปที่ user
 type Response struct {
 	PatientID string
 	FirstName string
@@ -17,6 +19,7 @@ type Response struct {
 	Age       int
 }
 
+// Patient struct ของผู้ป่วย
 type Patient struct {
 	ID        string `json:"id"`
 	PatientID string `json:"patient_id"`
@@ -25,6 +28,7 @@ type Patient struct {
 	Age       int    `json:"age"`
 }
 
+// CreatePatient สร้างผู้ป่วย
 func CreatePatient(c *gin.Context) {
 	var patient Patient
 	c.BindJSON(&patient)
